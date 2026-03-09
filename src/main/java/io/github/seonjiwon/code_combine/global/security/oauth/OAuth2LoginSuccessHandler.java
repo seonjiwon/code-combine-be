ecac64f444840ee7c23f2f4ce6cedfe4e8665491
seonjiwon <tommys915@gmail.com>
2026-03-09 14:58:09 +0900

    fix: url 수정

--- src/main/java/io/github/seonjiwon/code_combine/global/security/oauth/OAuth2LoginSuccessHandler.java
+++ src/main/java/io/github/seonjiwon/code_combine/global/security/oauth/OAuth2LoginSuccessHandler.java
@@ -72,7 +72,7 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         response.addCookie(cookie);
 
         // 6. 리다이렉트
-        String redirectUrl = frontendUrl + "/auth/callback";
+        String redirectUrl = "http://" + frontendUrl + "/auth/callback";
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
 
