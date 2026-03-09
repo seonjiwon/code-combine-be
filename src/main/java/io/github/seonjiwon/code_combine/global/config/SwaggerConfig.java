66abc9aa0f3d00ab1cafb9f5e9fd45e8b3392c03
seonjiwon <tommys915@gmail.com>
2026-03-09 14:08:42 +0900

    feat: swagger token 추가

--- src/main/java/io/github/seonjiwon/code_combine/global/config/SwaggerConfig.java
+++ src/main/java/io/github/seonjiwon/code_combine/global/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package io.github.seonjiwon.code_combine.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +16,19 @@ public class SwaggerConfig {
                 .title("Code Combine")
                 .description("코딩 테스트 리뷰 플랫폼").version("0.0.1");
 
+        SecurityScheme securityScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
+
+        // Security Requirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
         return new OpenAPI()
-                .info(info);
+            .info(info)
+            .addSecurityItem(securityRequirement)  // Security Requirement 추가
+            .schemaRequirement("BearerAuth", securityScheme);  // Security Scheme 추가
     }
 }
